e6dfb4c727bbf991debdc02b3c2b1dd4db56829c
Atul-Bhatt <atulbhatt61@gmail.com>
2021-03-16 20:55:50 +0530

    MongoDB

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,13 @@
 # importing the Flask class
 
+from application import routes
 from flask import Flask
+from config import Config
+from flask_mongoengine import MongoEngine
 
 # Passing the name of the current module to create a Flask object
 app = Flask(__name__)
+app.config.from_object(Config)
 
-from application import routes
+db = MongoEngine()
+db.init_app(app)
